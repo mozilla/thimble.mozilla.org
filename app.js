e4b054533559c07270d4354875f0452892a66750
Pomax <pomax@nihongoresources.com>
2013-04-11 13:40:33 -0400

    ...

--- app.js
+++ app.js
@@ -12,9 +12,8 @@ var express = require('express')
   , sanitize = require('htmlsanitizer');
 
 var app = express(),
-    nunjucksEnv;
-
-var ALLOWED_TAGS = [
+    nunjucksEnv,
+    ALLOWED_TAGS = [
       "!doctype", "html", "body", "a", "abbr", "address", "area", "article",
       "aside", "audio", "b", "base", "bdi", "bdo", "blockquote", "body", "br",
       "button", "canvas", "caption", "cite", "code", "col", "colgroup",
@@ -30,8 +29,6 @@ var ALLOWED_TAGS = [
       "title", "tr", "track", "u", "ul", "var", "video", "wbr"
     ],
     ALLOWED_ATTRS = {
-      // TODO: We should probably add to this. What meta attributes can't
-      // be abused for SEO purposes?
       "meta": ["charset", "name", "content"],
       "*": ["class", "id", "style"],
       "img": ["src", "width", "height"],
@@ -66,18 +63,19 @@ if ('development' == app.get('env')) {
 nunjucksEnv = new nunjucks.Environment(new nunjucks.FileSystemLoader('views'));
 nunjucksEnv.express(app);
 
+// ...
 app.get('/', function(request, response) {
   response.render('index.html');
 });
 
+// ...
 app.get('/projects/:name', function(request, response) {
   var tpl = nunjucksEnv.getTemplate('learning_projects/' + request.params.name + '.html' );
   var content = tpl.render({HTTP_STATIC_URL: '/learning_projects/'}).replace(/'/g, '\\\'').replace(/\n/g, '\\n');
   response.render('index.html', {template: content, HTTP_STATIC_URL: '/'});
 });
 
-app.get('/users', user.list);
-
+// run server
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
@@ -96,37 +94,82 @@ app.get("/remix", function(request, response) {
   response.end();
 });
 
-app.post('/publish', function(request, response) {
-  console.error("FUNCTION HIT");
-  console.error(request.body);
-  /*
-    1) get data from request
-    2) try to bleach it through http://htmlsanitizer.org/
-    3) if we succeeded in bleaching, we save that data to AWS
-
-    3b) for P.O.C., we're actually just going to say "hurray it worked" for now
-  */
-    var data = ( request.body.html ? request.body.html : "" );
-    sanitize({
-      url: 'http://htmlsanitizer.org',
-      text: data,
-      tags: ALLOWED_TAGS,
-      attributes: ALLOWED_ATTRS,
-      styles: [],
-      strip: false,
-      strip_comments: false
+/**
+ * PUBLISH DATA
+ *
+ * 1) get data from request
+ * 2) try to bleach it through http://htmlsanitizer.org/
+ * 3) if we succeeded in bleaching, we save that data to AWS
+ *  3b) for P.O.C., we're actually just going to say "hurray it worked" for now
+ */
+app.post('/publish', function(req, res) {
+  async.waterfall([
+    // do we have actual data to publish?
+    function(callback) {
+      var data = ( req.body.html ? req.body.html : "" );
+      if(data=="") { return callback("request had no publishable content"); }
+      callback(null, data);
     },
-    // response callback
-    function(err, sanitized) {
-      // At this point, we have a sanitized, and raw unsanitized
-      // data in "sanitized" and "buffer", respectively. We can now
-      // push this to AWS or wherever else we want
-      if(err) { response.end(); }
-      else {
-        var jsonRespose = { 'published-url' : 'http://mozilla.org/1' };
-        response.json(jsonRespose);
-        response.end();
-      }
-    });
-
+    // try to sanitize the raw data
+    function(data, callback) {
+      sanitize(
+        // sanitization options
+        { url: 'http://htmlsanitizer.org',
+          text: data,
+          tags: ALLOWED_TAGS,
+          attributes: ALLOWED_ATTRS,
+          styles: [],
+          strip: false,
+          strip_comments: false,
+          parse_as_fragment: false
+        },
+        function(err, sanitizedData) {
+          if(err) { return callback(err); }
+          callback(null, data, sanitizedData);
+        }
+      );
+    },
+    // try to get to our database
+    function(rawData, sanitizedData, callback) {
+      var db = new sqlite3.Database('thimble.sqlite', function(err) {
+        if(err!=null) { return callback(err); }
+        callback(null, db, rawData, sanitizedData);
+      });
+    },
+    // try to write the raw and sanitized data to the DB
+    function(db, rawData, sanitizedData, callback) {
+      db.serialize(function() {
+        // replace with http://stackoverflow.com/questions/1601151/how-do-i-check-in-sqlite-whether-a-table-exists or like
+        db.run("CREATE TABLE test (unsanitized TEXT, sanitized TEXT)",function(err) {
+          // don't care about errors here, atm.
+        });
+        db.run("INSERT INTO test VALUES (?, ?)", [rawData, sanitizedData], function(err) {
+          if(err!=null) { return callback(err); }
+          callback(null, db);
+        });
+      });
+    },
+    // get our new row number
+    function(db, callback) {
+      // NOTE: this totally doesn't work in async concurrent settings, so we need to get
+      // the rowid from the actual insert at some point (way) before deploy.
+      db.get("SELECT count(*) as totalCount FROM test", function(err, row) {
+        if(err!=null) { return callback(err); }
+        callback(null, db, row.totalCount);
+      });
+    },
+    // form a "load from..." URL based on our row number
+    function(db, resultId, callback) {
+      db.close();
+      var result = { 'published-url' : 'http://www.example.com/' + resultId };
+      callback(null, result);
+    }
+  ],
+  // final callback
+  function (err, result) {
+   console.error("end");
+    if(err) { res.send("could not publish, "+err); }
+    else { res.json(result); }
+    res.end();
+  });
 });
