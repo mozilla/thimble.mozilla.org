ac5e71c93c283fcfcc44437deb04b60279558222
Pomax <pomax@nihongoresources.com>
2013-04-15 17:40:50 -0400

    reeeefactor, based on jbuck's node-express advice

--- app.js
+++ app.js
@@ -3,83 +3,48 @@ const DEVELOPMENT = "development";
 /**
  * Module dependencies.
  */
-var express = require('express')
-  , nunjucks = require('nunjucks')
-  , routes = require('./routes')
-  , habitat = require('habitat')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path')
-  , ajax = require('request')
-  , sanitize = require('htmlsanitizer')
-  , sqlite = require('sqlite3')
-  , async = require('async')
-  , fs = require('fs');
+var express = require('express'), 
+    nunjucks = require('nunjucks'),
+    routes = require('./routes'),
+    habitat = require('habitat'),
+    user = require('./routes/user'),
+    http = require('http'),
+    path = require('path'),
+    ajax = require('request'),
+    sqlite = require('sqlite3'),
+    async = require('async'),
+    fs = require('fs'),
+    middleware = require( "./lib/middleware" );
 
 habitat.load();
 
 var app = express(),
-    nunjucksEnv,
-    // whitelist for HTML5 elements
-    ALLOWED_TAGS = [
-      "!doctype", "html", "body", "a", "abbr", "address", "area", "article",
-      "aside", "audio", "b", "base", "bdi", "bdo", "blockquote", "body", "br",
-      "button", "canvas", "caption", "cite", "code", "col", "colgroup",
-      "command", "datalist", "dd", "del", "details", "dfn", "div", "dl", "dt",
-      "em", "embed", "fieldset", "figcaption", "figure", "footer", "form",
-      "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hgroup", "hr",
-      "html", "i", "iframe", "img", "input", "ins", "keygen", "kbd", "label",
-      "legend", "li", "link", "map", "mark", "menu", "meta", "meter", "nav", 
-      "noscript", "object", "ol", "optgroup", "option", "output", "p", "param",
-      "pre", "progress", "q", "rp", "rt", "s", "samp", "section", "select",
-      "small", "source", "span", "strong", "style", "sub", "summary", "sup", 
-      "table", "tbody", "td", "textarea", "tfoot", "th", "thead", "time",
-      "title", "tr", "track", "u", "ul", "var", "video", "wbr"
-    ],
-    // whitelist for HTML5 element attributes.
-    ALLOWED_ATTRS = {
-      "meta": ["charset", "name", "content"],
-      "*": ["class", "id", "style"],
-      "img": ["src", "width", "height"],
-      "a": ["href"],
-      "base": ["href"],
-      "iframe": ["src", "width", "height", "frameborder", "allowfullscreen"],
-      "video": ["controls", "autoplay", "preload", "loop", "mediaGroup", "src",
-                "poster", "muted", "width", "height"],
-      "audio": ["controls", "autoplay", "preload", "loop", "src"],
-      "source": ["src", "type"],
-      "link": ["href", "rel", "type"]
-    };
+    nunjucksEnv = new nunjucks.Environment(new nunjucks.FileSystemLoader('views')),
+    env = new habitat();
 
-var env = new habitat();
+nunjucksEnv.express(app);
 
 // all environments
 app.use(express.favicon());
 app.use(express.logger('dev'));
+app.use(express.compress());
 app.use(express.bodyParser());
 app.use(express.cookieParser());
 app.use(express.cookieSession({secret: env.get('secret')}));
-app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'learning_projects')));
 
-// set up personal
-require('express-persona')(app, { audience: env.get });
+// set up persona
+require('express-persona')(app, { audience: env.get("HOSTNAME") });
 if (env.get("NODE_ENV") === DEVELOPMENT) {
   app.use(express.errorHandler());
 }
-
-// Tell express where to find our templates.
-nunjucksEnv = new nunjucks.Environment(new nunjucks.FileSystemLoader('views'));
-nunjucksEnv.express(app);
-
 
 // base dir lookup
 app.get('/', function(req, res) {
   res.render('index.html', { appURL: env.get("HOSTNAME") } );
 });
-
 
 // learning project listing
 app.get('/projects', function(req, res) {
@@ -95,266 +60,80 @@ app.get('/projects', function(req, res) {
   });
 });
 
-
 // learning project lookup
 app.get('/projects/:name', function(req, res) {
+  // This is quite ugly, and we need a better way to inject data
+  // into friendlycode. I'm pretty sure it CAN load from URI, we
+  // just need to find out how to tell it to...
   var tpl = nunjucksEnv.getTemplate('learning_projects/' + req.params.name + '.html' );
   var content = tpl.render({HTTP_STATIC_URL: '/learning_projects/'}).replace(/'/g, '\\\'').replace(/\n/g, '\\n');
   res.render('index.html', {appURL: env.get("HOSTNAME"), template: content, HTTP_STATIC_URL: '/'});
 });
 
-
 // "my projects" listing -- USED FOR DEV WORK ATM, MAY NOT BE PERMANENT IN ANY WAY
-app.get('/myprojects', function(req, res) {
-  async.waterfall([
-    // are we logged in?
-    function(callback) {
-      // logged in?
-      var personaId = req.session.email;
-      if(personaId==null) { return callback("you'll have to log in to see your published projects"); }
-      callback(null, personaId);
-    },
-
-    // try to get to our database
-    function(personaId, callback) {
-      var db = new sqlite.Database('thimble.sqlite', function(err) {
-        if(err!=null) { return callback(err); }
-        callback(null, db, personaId);
-      });
-    },
-
-    // grab this project's data from the db
-    function(db, personaId, callback) {
-      db.serialize(function() {
-        db.all("SELECT rowid FROM test WHERE personaid = ?", [personaId], function(err, rows) {
-          if(err!=null) { return callback(err); }
-          callback(null, db, rows);
+app.get('/myprojects',
+  middleware.checkForPersonaAuth,
+  function(req, res) {
+    var db = new sqlite.Database('thimble.sqlite', function(err) {
+      if(err) { res.send(err); res.end(); return; }
+      db.all("SELECT rowid FROM test WHERE personaid = ?", [req.session.email], function(err, rows) {
+        if(err) { res.send(err); res.end(); return; }
+        var response = "<h1>MY PROJECTS TEMPLATE GOES HERE</h1>\n", id;
+        rows.forEach( function(row) {
+          id = row.rowid;
+          response += "<a href='/remix/"+id+"'>"+id+"</a> (<a href='/remix/"+id+"/edit'>edit</a>)<br>\n";
         });
+        res.send(response);
+        res.end();
       });
-    },
+    });
+  }
+);
 
-    // we're done, close the db
-    function(db, rows, callback) {
-      db.close();
-      callback(null, rows);
-    }
-  ],
-
-  // final callback
-  function (err, rows) {
-    if(err) { res.send("could not fetch content, "+err); }
-    else {
-      var response = "<h1>MY PROJECTS TEMPLATE GOES HERE</h1>\n", id;
-      rows.forEach( function(row) {
-        id = row.rowid;
-        response += "<a href='/remix/"+id+"'>"+id+"</a> (<a href='/remix/"+id+"/edit'>edit</a>)<br>\n";
-      });
-      res.send(response);
-    }
-    res.end();
+// what do we do when a project request comes in by id (:id route)?
+app.param('id', function(req, res, next, id) {
+  var db = new sqlite.Database('thimble.sqlite', function(err) {
+    if(err) { return next(err); }
+    db.get("SELECT * FROM test WHERE rowid = ?", [id], function(err, row) {
+      if(err) { return next(err); }
+      req.pageData = row.sanitized;
+      next();
+    });
   });
 });
 
-
-// load up someone's project for editing (from db)
+// remix a published page (from db)
 app.get("/remix/:id/edit", function(req, res) {
-  async.waterfall([
-    // do we have a project to work with?
-    function(callback) {
-      var id = (req.params.id | 0);
-      if(id === 0) { return callback("request did not point to a project"); }
-      callback(null, id);
-    },
-
-    // try to get to our database
-    function(id, callback) {
-      var db = new sqlite.Database('thimble.sqlite', function(err) {
-        if(err!=null) { return callback(err); }
-        callback(null, id, db);
-      });
-    },
-
-    // grab this project's data from the db
-    function(id, db, callback) {
-      db.serialize(function() {
-        db.get("SELECT * FROM test WHERE rowid = ?", [id], function(err, row) {
-          if(err!=null) { return callback(err); }
-          // FIXME: note that we're not pulling up the "real" raw data. we're still grabbing sanitized data.
-          callback(null, db, row.sanitized);
-        });
-      });
-    },
-
-    // we're done, close the db
-    function(db, data, callback) {
-      db.close();
-      callback(null, data);
-    }
-  ],
-
-  // final callback
-  function (err, content) {
-    if(err) { res.send("could not publish, "+err); }
-    else if(!content) { res.send("could not publish: content was empty"); }
-    else {
-      // load up the content for mixing.
-      content = content.replace(/'/g, '\\\'').replace(/\n/g, '\\n');
-      res.render('index.html', {appURL: env.get("HOSTNAME"), template: content, HTTP_STATIC_URL: '/'});
-    }
-    res.end();
+  // This is quite ugly, and we need a better way to inject data
+  // into friendlycode. I'm pretty sure it CAN load from URI, we
+  // just need to find out how to tell it to...
+  var content = req.pageData.replace(/'/g, '\\\'').replace(/\n/g, '\\n');
+  res.render('index.html', {
+    appURL: env.get("HOSTNAME"),
+    template: content, HTTP_STATIC_URL: '/'
   });
 });
-
 
 // view a published page (from db)
 app.get("/remix/:id", function(req, res) {
-  async.waterfall([
-    // do we have a project to work with?
-    function(callback) {
-      var id = (req.params.id | 0);
-      if(id === 0) { return callback("request did not point to a project"); }
-      callback(null, id);
-    },
-
-    // try to get to our database
-    function(id, callback) {
-      var db = new sqlite.Database('thimble.sqlite', function(err) {
-        if(err!=null) { return callback(err); }
-        callback(null, id, db);
-      });
-    },
-
-    // grab this project's data from the db
-    function(id, db, callback) {
-      db.serialize(function() {
-        db.get("SELECT * FROM test WHERE rowid = ?", [id], function(err, row) {
-          if(err!=null) { return callback(err); }
-          callback(null, db, row.sanitized);
-        });
-      });
-    },
-
-    // we're done, close the db
-    function(db, data, callback) {
-      db.close();
-      callback(null, data);
-    }
-  ],
-
-  // final callback
-  function (err, result) {
-    if(err) { res.send("could not publish, "+err); }
-    else { res.send(result); }
-    res.end();
-  });
+  res.send(req.pageData);
+  res.end();
 });
-
 
 // publish a remix (to the db)
-app.post('/publish', function(req, res) {
-  async.waterfall([
-
-    // do we have actual data to publish?
-    function(callback) {
-      // logged in?
-      var personaId = req.session.email;
-      if(personaId==null) { return callback("you'll have to log in to publish"); }
-      // is there data?
-      var data = ( req.body.html ? req.body.html : "" );
-      if(data=="") { return callback("request had no publishable content"); }
-      // is there a "we are remixing id ... " indicator
-      var originalRecord = req.body['original-url'];
-      if(originalRecord) {
-        originalRecord = originalRecord.substring(originalRecord.lastIndexOf('/')+1);
-      };
-      callback(null, personaId, data, originalRecord);
-    },
-
-    // try to sanitize the raw data
-    function(personaId, data, originalRecord, callback) {
-      sanitize( {
-        endpoint: env.get("BLEACH_ENDPOINT"),
-        text: data,
-        tags: ALLOWED_TAGS,
-        attributes: ALLOWED_ATTRS,
-        styles: [],
-        strip: false,
-        strip_comments: false,
-        parse_as_fragment: false,
-      }, function(err, sanitizedData) {
-        if(err) { return callback(err); }
-        callback(null, personaId, data, sanitizedData, originalRecord);
-      });
-    },
-
-    // try to get to our database
-    function(personaId, rawData, sanitizedData, originalRecord, callback) {
-      var db = new sqlite.Database('thimble.sqlite', function(err) {
-        if(err!=null) { return callback(err); }
-        callback(null, db, personaId, rawData, sanitizedData, originalRecord);
-      });
-    },
-
-    // try to write the raw and sanitized data to the DB
-    function(db, personaId, rawData, sanitizedData, originalRecord, callback) {
-      db.serialize(function() {
-        // replace with http://stackoverflow.com/questions/1601151/how-do-i-check-in-sqlite-whether-a-table-exists or like
-        db.run("CREATE TABLE IF NOT EXISTS test (personaid TEXT, raw TEXT, sanitized TEXT)", function(err) {
-          if(err!=null) return callback(err);
-
-          // do we own this remix? if so, update. Otherwise, write.
-          db.get("SELECT count(*) as count FROM test WHERE rowid = ? AND personaid = ?",
-            [originalRecord, personaId],
-            function(err, row) {
-              if(err!=null) { return callback(err); }
-
-              // if we don't own [originalRecord], write a new entry:
-              if(row.count == 0 ) {
-                db.run("INSERT INTO test VALUES (?, ?, ?)",
-                  [personaId, rawData, sanitizedData],
-                  function(err, result) {
-                    if(err!=null) { return callback(err); }
-                    callback(null, db, this.lastID);
-                  }
-                );
-              }
-
-              // otherwise, update it with this new content:
-              else {
-                db.run("UPDATE test SET raw = ?, sanitized = ? WHERE rowid = ?",
-                  [rawData, sanitizedData, originalRecord],
-                  function(err, result) {
-                    if(err!=null) { return callback(err); }
-                    callback(null, db, originalRecord);
-                  }
-                );
-              }
-            }
-          );
-        });
-      });
-    },
-
-    // form a "load from..." URL based on our row number
-    function(db, resultId, callback) {
-      db.close();
-      var result = { 'published-url' : 'http://www.example.com/' + resultId };
-      callback(null, result);
-    }
-  ],
-
-  // final callback
-  function (err, result) {
-    debugger;
-    if(err) { res.send("could not publish, "+err); }
-    else { res.json(result); }
+app.post('/publish',
+         middleware.checkForPersonaAuth,
+         middleware.checkForPublishData,
+         middleware.checkForOriginalPage,
+         middleware.bleachData(env.get("BLEACH_ENDPOINT")),
+         middleware.publishData(sqlite),
+  function(req, res) {
+    res.json({ 'published-url' : env.get('HOSTNAME') + '/' + req.publishId });
     res.end();
-  });
-});
-
+  }
+);
 
 // run server
 http.createServer(app).listen(env.get("PORT"), function(){
   console.log('Express server listening on port ' + env.get("PORT"));
 });
-
