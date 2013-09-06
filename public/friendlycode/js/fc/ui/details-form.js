b45aa81968efd2d4c1fd1e24f96d1a7b28e2922b
Kate Hudson <k88hudson@gmail.com>
2013-09-06 09:22:44 -0400

    bug 913006: Create tags on blur

--- public/friendlycode/js/fc/ui/details-form.js
+++ public/friendlycode/js/fc/ui/details-form.js
@@ -37,10 +37,13 @@ define(['template!details-form'], function (detailsFormHTML) {
 
     // Setup
     $input('tag-input').on('keydown', function (e) {
-      if (e.which === 13 || e.which === 188 || e.which === 9) {
+      if (e.which === 13 || e.which === 188 ) {
         e.preventDefault();
         self.addTags(this.value);
       }
+    });
+    $input('tag-input').on('blur', function (e) {
+      self.addTags(this.value);
     });
     $input('tag-output').click(function (e) {
       if (e.target.tagName === 'LI') {
