1ae31adf55074dedd948acef8534632444c02607
Matthew Schranz <schranz.m@gmail.com>
2013-09-09 18:09:58 -0400

    Bug 913973 - Fix security vulnerability when using express.bodyParser()

--- app.js
+++ app.js
@@ -74,7 +74,8 @@ if (!!env.get("FORCE_SSL") ) {
   app.enable("trust proxy");
 }
 app.use(express.compress());
-app.use(express.bodyParser());
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(express.cookieParser());
 app.use(express.cookieSession({
   key: "thimble.sid",
