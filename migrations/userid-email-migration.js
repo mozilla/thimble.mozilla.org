e8ba28e21963c5c26209a626023f4a3f8925f22e
Christopher De Cairos <cade@mozillafoundation.org>
2014-06-11 12:21:42 -0400

    better migration

--- migrations/userid-email-migration.js
+++ migrations/userid-email-migration.js
@@ -19,93 +19,45 @@ var dbConfig = env.get( "DB" ),
       endpoint: env.get( "LOGIN_URL_AUTH" )
     }),
     db = require( "../lib/database" ),
-    dbAPI = db( "thimbleproject", dbConfig ),
-    emailMap = {},
-    processed = 0,
-    updated = 0,
-    limit = 50,
-    lastpid = 0;
+    dbAPI = db( "thimbleproject", dbConfig );
 
-function getProjectEmailSet( callback ) {
-  dbAPI.model.findAll({
-    where: "email IS NOT NULL",
-    group: "email",
-    attributes: ["email"]
-  }).success(function(hits) {
-    async.eachLimit( hits, 5, function emailIterator( hit, callback ) {
-      client.get.byEmail( hit.email, function( err, resp ) {
-        if ( err ) {
-          return callback( err );
-        }
-        if ( !resp || !resp.user ) {
-          console.log( "no user account found for: " + hit.email + ".." );
-          return callback();
-        }
-        emailMap[ hit.email ] = resp.user.id;
-        callback();
-      });
-    }, function complete( err ) {
-      if ( err ) {
-        console.error( err );
-        process.exit( 1 );
-      }
-      console.log( "Email - userid map generated, Fetching and updating projects...." );
-      callback();
-    })
-  })
-}
-
-function setUserid( project, callback ) {
-  lastpid = project.id;
-  if ( !emailMap[ project.email ] ) {
-    console.log("no account exists for project owner, skipping.");
-    return callback();
-  }
-
-  project.userid = emailMap[ project.email ];
-  project.save(["userid"]).error(callback).success(function() {
-    updated++;
-    console.log( project.id + " successfully updated with userid " + project.userid );
-    callback();
-  });
-}
-
-function getNextSet( callback ) {
-  dbAPI.model.findAll({
-    where: {
-      id: {
-        gt: lastpid
-      },
-      userid: null
-    },
-    limit: limit,
-    order: "id ASC"
-  }).complete(function( err, projects ) {
-    async.eachSeries( projects, setUserid, callback );
-  });
-}
-
-function migrate() {
-  dbAPI.model.count({
-    where: {
-      userid: null
+var q = async.queue(function( task, callback ) {
+  client.get.byEmail(task.email, function(err, res) {
+    if (err) {
+      return callback(err);
     }
-  }).success(function( count ) {
-    console.log( "Found " + count + " projects without userid..." );
-    async.doWhilst( getNextSet, function() {
-      processed += limit;
-      if ( processed < count ) {
-        return true
-      }
-      return false;
-    }, function( err ) {
-      if ( err ) {
-        console.error( "Error: ", err );
-      }
-      console.log( updated + " Projects updated." );
-      process.exit( 0 );
-    });
-  })
-}
 
-getProjectEmailSet( migrate );
+    if (!res || !res.user) {
+      console.log("No user found for email %s", task.email);
+      return callback();
+    }
+
+    console.log("Set userid = %s where email = %s", res.user.id, task.email);
+    dbAPI.model.update({
+      userid: res.user.id
+    }, {
+      email: task.email
+    }).then(function(affectedRows) {
+      callback();
+    });
+  });
+});
+
+q.drain = function() {
+  console.log( "migration complete!" );
+};
+
+dbAPI.model.findAll({
+  where: {
+    userid: null,
+    email: {
+      not: "NULL"
+    }
+  },
+  group: "email",
+  attributes: ["email"]
+}, {
+  raw: true
+}).success(function(hits) {
+  q.push(hits);
+});
