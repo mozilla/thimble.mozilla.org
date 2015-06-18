aee80f3762162e8ca0675bab0b4b566c6526c8ae
Kieran Sedgwick <kieran.sedgwick@gmail.com>
2015-06-18 16:16:03 -0400

    Fixed reference to brackets host

--- public/friendlycode/js/fc/ui/bramble-proxy.js
+++ public/friendlycode/js/fc/ui/bramble-proxy.js
@@ -102,7 +102,7 @@ define(["backbone-events", "fc/prefs", "fc/bramble-ui-bridge"],
     this.init = function(make) {
       // Start loading Bramble
       Bramble.load("#webmaker-bramble",{
-        url: options.editorURL
+        url: options.editorUrl
       });
 
       // Event listeners
