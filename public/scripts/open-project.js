553787538454a3d180dedea825428c3882e697b0
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2015-08-06 15:15:03 -0400

    Fix #680 - Add (MVP level) confirmation before deleting a project

--- public/scripts/open-project.js
+++ public/scripts/open-project.js
@@ -62,6 +62,11 @@ require(["jquery"], function($) {
   });
 
   $("td.project-delete").click(function() {
+    // TODO: we can do better than this, but let's at least make it harder to lose data.
+    if(!confirm("OK to Delete this project?")) {
+      return;
+    }
+
     var project = $(this).parent();
     $(this).text("Deleting...");
 
