caac67d28222b857ee1e61a30865c4397700e307
Gideon Thomas <r.gideonthomas@gmail.com>
2015-08-28 13:31:11 -0400

    Fix #950 follow-up - Fix unauthorized error when logging in and out more than once

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -11,5 +11,9 @@ module.exports = function(config, req, res) {
   var loginType = "&action=" + (req.query.signup ?  "signup" : "signin");
   var state = "&state=" + req.cookies.state;
 
-  res.redirect(301, config.loginURL + state + loginType);
+  res.set({
+    "Cache-Control": "no-cache"
+  });
+
+  res.redirect(307, config.loginURL + state + loginType);
 };
