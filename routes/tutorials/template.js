a18cddd994ce072b32d409b7b58d8744c526b04f
Kieran Sedgwick <kieran.sedgwick@gmail.com>
2015-09-10 11:29:37 -0400

    Fixed #1084 - Fixed broken reference to the tutorial style guide

--- routes/tutorials/template.js
+++ routes/tutorials/template.js
@@ -1,9 +1,3 @@
 module.exports = function(config, req, res) {
   res.render("tutorial/template.html", {appURL: config.appURL});
 };
-
-exports.styleGuide = function(config) {
-  return function(req, res) {
-    res.render("tutorial/style-guide.html", {appURL: config.appURL});
-  };
-};
