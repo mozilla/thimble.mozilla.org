62f830c64b45331470e32dc1b4ae39ad484038cd
Luke Pacholski <luke.pacholski@gmail.com>
2015-09-17 13:36:54 -0700

    Added 3 new projects to the homepage

--- routes/main/homepage.js
+++ routes/main/homepage.js
@@ -15,6 +15,12 @@ module.exports = function(config, req, res) {
     backToSchoolPublishedUrl: env.get("BACK_TO_SCHOOL_PUBLISHED_URL"),
     comicStripRemixUrl: env.get("COMIC_STRIP_REMIX_URL"),
     comicStripPublishedUrl: env.get("COMIC_STRIP_PUBLISHED_URL"),
+    threeThingsIHeartRemixUrl: env.get("THREE_THINGS_I_HEART_REMIX_URL"),
+    threeThingsIHeartKitUrl: env.get("THREE_THINGS_I_HEART_KIT_URL"),
+    homeworkExcuseGeneratorRemixUrl: env.get("HOMEWORK_EXCUSE_GENERATOR_REMIX_URL"),
+    homeworkExcuseGeneratorKitUrl: env.get("HOMEWORK_EXCUSE_GENERATOR_KIT_URL"),
+    sixWordSummerRemixUrl: env.get("SIX_WORD_SUMMER_REMIX_URL"),
+    sixWordSummerKitUrl: env.get("SIX_WORD_SUMMER_KIT_URL"),
     loginURL: config.appURL + "/login",
     editorHOST: config.editorHOST,
     editorURL: config.editorURL
