0e54f885dfa779cc3677b66965a4d86e73f2f9a7
Luke Pacholski <luke.pacholski@gmail.com>
2015-10-01 15:27:43 -0500

    Modifying our keypress listeners to work with "any" key, not just specific ones so that we can measure the length of the project title as someone is editing it.

--- public/editor/scripts/editor/js/fc/project-rename.js
+++ public/editor/scripts/editor/js/fc/project-rename.js
@@ -44,6 +44,10 @@ define(function(require) {
         esc: new KeyHandler.ESC(container, function() {
           context.saveButton.off("click", saveClicked);
           editingComplete(context);
+        }),
+        any: new KeyHandler.Any(container, function() {
+          var input = context.titleBar._element;
+          var nameLength = $(input).val().length;
         })
       };
 
