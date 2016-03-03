e92cbf7c2be8e0f120161e84987050f9c567ee73
Gideon Thomas <gideon@mozillafoundation.org>
2016-03-03 11:28:31 -0500

    Fix unnecessary mapping of some locales to other locales

--- server/localize.js
+++ server/localize.js
@@ -1,7 +1,6 @@
 "use strict";
 
 let webmakerI18N = require("webmaker-i18n");
-let webmakerLocaleMapping = require("webmaker-locale-mapping");
 let path = require("path");
 let bestlang = require("bestlang");
 
@@ -14,7 +13,6 @@ module.exports = function localize(server, options) {
   server.use(webmakerI18N.middleware({
     supported_languages: options.supported_languages,
     default_lang: "en-US",
-    mappings: webmakerLocaleMapping,
     translation_directory: path.resolve(root, options.locale_dest)
   }));
 
