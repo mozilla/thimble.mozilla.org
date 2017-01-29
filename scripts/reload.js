9bc2a650217fdbaf35c2f373d3702c253e7fdb9e
Aayush Sanghavi <aayushsanghavi26@gmail.com>
2017-01-29 18:19:12 +0530

    Removed dependence on jQuery
    Using window event handler - beforeunload instead of jQuery to intercept any action causing to leave the page

--- scripts/reload.js
+++ scripts/reload.js
@@ -1,21 +1,5 @@
-var pressed={};
-var lifted={};
-
-$(document).on("keydown", function(e){
-	e = e || window.event;
-	pressed[e.keyCode] = true;
-	lifted[e.keyCode] = false;	
-	check();
+window.addEventListener("beforeunload", function (e) {
+	var confirmationMessage = "Are you sure?";
+	e.returnValue = confirmationMessage;
+	return confirmationMessage;
 });
-
-$(document).on("keyup", function(e){
-	e = e || window.event;
-	lifted[e.keyCode] = true;
-	pressed[e.keyCode] = false;
-});
-
-function check() {
-	if (pressed[17] == true && lifted[17] == false && pressed[82] == true) {
-		alert("Are you sure you want to leave this page?");
-	}
-}
