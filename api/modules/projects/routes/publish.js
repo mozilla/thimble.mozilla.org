7ab02a65d71283aebc4cf1c3ddabb619355d4a9d
Gideon Thomas <gideon@mozillafoundation.org>
2017-02-08 16:33:30 -0500

    Squashed 'services/publish.webmaker.org/' changes from 080e741..6a16604
    6a16604 Set readonly on projects during publish (#238)
    
    git-subtree-dir: services/publish.webmaker.org
    git-subtree-split: 6a16604c467540496c55bbd7137336ff88a81263

--- api/modules/projects/routes/publish.js
+++ api/modules/projects/routes/publish.js
@@ -4,8 +4,12 @@ const Prerequisites = require(`../../../classes/prerequisites`);
 
 const ProjectsModel = require(`../model`);
 const projectsController = require(`../controller`);
+const schema = require(`../schema`).publishQuery;
 
 module.exports = [{
+  // Query String params allowed:
+  // - readonly : (Optional) Its value can either be true or false.
+  // Sets the readonly property of projects before publishing.
   method: `PUT`,
   path: `/projects/{id}/publish`,
   config: {
@@ -18,7 +22,10 @@ module.exports = [{
       Prerequisites.validateOwnership()
     ],
     handler: projectsController.publish.bind(projectsController),
-    description: `Publish a project.`
+    description: `Publish a project.`,
+    validate: {
+      query: schema
+    }
   }
 }, {
   method: `PUT`,
