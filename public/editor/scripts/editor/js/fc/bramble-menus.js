948deb0b3231947aae366deaebb58e769222ffc2
peiying16 <mryang91@hotmail.com>
2017-02-10 19:21:41 -0500

    Fix #1631- remember allow javaScript setting (#1725)
    * Init Allow JavaScript
    
    * Change comment

--- public/editor/scripts/editor/js/fc/bramble-menus.js
+++ public/editor/scripts/editor/js/fc/bramble-menus.js
@@ -54,8 +54,12 @@ define(function(require) {
     });
     // Set initial UI value to match editor value
     setWordWrapUI(bramble.getWordWrap());
-
-
+    //set initial UI value to allow javascript UI
+    if(bramble.getAllowJavaScript()) {
+        $("#allow-scripts-toggle").addClass("switch-enabled");
+    } else {
+        $("#allow-scripts-toggle").removeClass("switch-enabled");
+    }
     // Enable/Disable JavaScript in Preview
     $("#allow-scripts-toggle").click(function() {
       // Toggle current value
