586c24bf9eaa642fb4632f50f1432267a7593779
Jack <Jejames1@myseneca.ca>
2017-03-05 12:11:09 -0500

    Fixed Issue #1397, now you no longer need to restart the server to see HTML changes

--- server/templatize.js
+++ server/templatize.js
@@ -3,7 +3,7 @@
 let Nunjucks = require("nunjucks");
 
 module.exports = function templatize(server, paths) {
-  let engine = new Nunjucks.Environment(paths.map(path => new Nunjucks.FileSystemLoader(path)), { autoescape: true });
+  let engine = new Nunjucks.Environment(paths.map(path => new Nunjucks.FileSystemLoader(path, { noCache: true, watch: true } )), { autoescape: true });
 
   engine.addFilter("instantiate", function(input) {
     return (new Nunjucks.Template(input)).render(this.getVariables());
