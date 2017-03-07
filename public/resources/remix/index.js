678f8e66d44233893a0cfb4719c547cf56d17768
Aayush Sanghavi <aayushsanghavi26@gmail.com>
2017-03-07 16:14:15 +0530

    Changed jQuery dependent code to plain JS

--- public/resources/remix/index.js
+++ public/resources/remix/index.js
@@ -6,7 +6,7 @@
 
   var gaTrackingId = 'UA-68630113-1';
 
-  function injectAnalytics($) {
+  function injectAnalytics() {
     var analyticsHtml =
     '\n<script>\n' +
     '(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){\n' +
@@ -17,111 +17,121 @@
     'ga(\'send\', \'pageview\');\n' +
     '</script>\n';
 
-    $(head).append(analyticsHtml);
+    head.appendChild(analyticsHtml);
   }
 
-  function setupBar(err, $) {
+  function setupBar(err) {
     if(err) {
       console.log("[Thimble Error] Unable to inject Remix UI. Error was: `" + err + "`");
       return;
     }
 
     var isTouchDevice = 'ontouchstart' in document.documentElement;
-    var detailsBar = $(".details-bar");
-    detailsBar.attr("style", "");
+    var detailsBar = document.getElementsByClassName("details-bar");
+    detailsBar.setAttribute("style", "");
 
     if(isTouchDevice) {
-      detailsBar.addClass("touch-mode");
-    } else {
-      detailsBar.addClass("mouse-mode");
+      detailsBar.classList.add("touch-mode");
+    }
+    else {
+      detailsBar.classList.add("mouse-mode");
     }
 
-    detailsBar.on("click", ".thimble-button",function(){
-      detailsBar.removeClass("collapsed");
+    detailsBar.on("click", ".thimble-button", function(){
+      detailsBar.classList.remove("collapsed");
       return false;
     });
 
     detailsBar.on("click", ".close-details-bar", function(){
-      detailsBar.addClass("collapsed");
+      detailsBar.classList.add("collapsed");
       return false;
     });
 
-    $(".mouse-mode").on("mouseenter",function(){
-      detailsBar.removeClass("collapsed");
+    document.getElementsByClassName("mouse-mode").on("mouseenter",function(){
+      detailsBar.classList.remove("collapsed");
     });
 
-    $(".mouse-mode").on("mouseleave",function(){
-      detailsBar.addClass("collapsed");
+    document.getElementsByClassName("mouse-mode").on("mouseleave",function(){
+      detailsBar.classList.add("collapsed");
     });
   }
 
-  function injectDetailsBar($, metadata, callback) {
-    $.ajax({
-      url: metadata.host + "/projects/remix-bar",
-      cache: false,
-      accepts: "text/html",
-      dataType: "html",
-      data: {
-        host: metadata.host,
-        id: metadata.projectId,
-        title: metadata.projectTitle,
-        author: metadata.projectAuthor,
-        updated: metadata.dateUpdated
+  function injectDetailsBar(metadata, callback) {
+    var xmlhttp = new XMLHttpRequest();
+    var url = metadata.host + "/projects/remix-bar";
+    var data = {
+      host: metadata.host,
+      id: metadata.projectId,
+      title: metadata.projectTitle,
+      author: metadata.projectAuthor,
+      updated: metadata.dateUpdated
+    };
+
+    xmlhttp.onreadystatechange = function() {
+      if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+        if (xmlhttp.status == 200) {
+          body.insertBefore(response, body.firstChild);
+	    callback(null);
+        }
+        else if (xmlhttp.status == 400) {
+	    callback(textStatus);
+        }
       }
-    })
-    .done(function(response) {
-      $(response).prependTo("body");
-      callback(null, $);
-    })
-    .fail(function(jqXHR, textStatus) {
-      callback(textStatus);
-    });
+    };
+
+    xmlhttp.open("POST", url, true);
+    xmlhttp.send(data);
   }
 
-  function injectStyleSheets($, metadata) {
+  function injectStyleSheets(metadata) {
     var stylesheets =
       "<link href=\"" + metadata.host + "/resources/remix/clean-slate.min.css\" rel=\"stylesheet\">\n" +
       "<link href=\"" + metadata.host + "/resources/remix/style.css\" rel=\"stylesheet\">\n";
-    $("head").append(stylesheets);
+    document.getElementsByTagName("head").appendChild(stylesheets);
   }
 
-  function getMetadata($) {
-    var metadata = {};
-    var metaTags = $("meta");
+  function grep(items, callback) {
+    var filtered = [], len = items.length, i = 0;
+    for (i; i < len; i++) {
+      var item = items[i];
+      var cond = callback(item);
+      if (cond) {
+        filtered.push(item);
+      }
+    }
+    return filtered;
+  }
 
-    $.grep(metaTags, function(elem, index) {
-      return /^data-remix-.+/.test($(metaTags[index]).attr("name"));
+  function getMetadata() {
+    var metadata = {};
+    var metaTags = document.getElementsByTagName("meta");
+
+    var set = grep(metaTags, function(metaTag) {
+      return /^data-remix-.+/.test((metaTag).getAttribute("name"));
     })
     .forEach(function(metaTag) {
-      metaTag = $(metaTag);
-      var key = metaTag.attr("name").replace(/^data-remix-/, "");
+      var key = metaTag.getAttribute("name").replace(/^data-remix-/, "");
+
       // We see if a value for the current attribute already exists.
       // If it does, that means that we already encountered a meta tag
       // with that name which will refer to a more recent value than the
       // current one (we add the most recent values above all other values in
       // the <head> tag's contents).
-      metadata[key] = metadata[key] || metaTag.attr("content");
+      metadata[key] = metadata[key] || metaTag.getAttribute("content");
     });
 
     return metadata;
   }
 
   function run() {
-    var $$ = $.noConflict(true);
-    $$(document).ready(function($) {
-      var metadata = getMetadata($);
-      injectAnalytics($);
-      injectStyleSheets($, metadata);
-      injectDetailsBar($, metadata, setupBar);
-    });
+    document.onreadystatechange = function () {
+      if (document.readyState === "complete") {
+        var metadata = getMetadata();
+        injectAnalytics();
+        injectStyleSheets(metadata);
+        injectDetailsBar(metadata, setupBar);
+      }
+    }
   }
-
-  (function(doc, script) {
-    script = doc.createElement("script");
-    script.type = "text/javascript";
-    script.onload = run;
-    script.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js";
-    doc.getElementsByTagName("head")[0].appendChild(script);
-  }(document));
-
+  
 }(document, document.head));
