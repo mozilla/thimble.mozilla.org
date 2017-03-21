a23d523979ab953f074ec8544c03e651bbaa2bfc
Aayush Sanghavi <aayushsanghavi26@gmail.com>
2017-03-21 22:23:06 +0530

    Using .filter instead of the grep function, classList.contains instead of .className, fixed some indentation and calling event.stopPropagation instead of returning false

--- public/resources/remix/index.js
+++ public/resources/remix/index.js
@@ -7,10 +7,9 @@
   var gaTrackingId = 'UA-68630113-1';
 
   function injectAnalytics() {
-    var analyticsHtml = document.createElement("script");
-    analyticsHtml.type = "text/javascript";
-    analyticsHtml.innerHTML = '(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){\n' + '(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n' + 'm=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n' + '})(window,document,\'script\',\'//www.google-analytics.com/analytics.js\',\'ga\');\n' + 'ga(\'create\', \'' + gaTrackingId + '\', \'auto\');\n' + 'ga(\'send\', \'pageview\');\n';
-    head.appendChild(analyticsHtml);
+    var analytics = document.createElement("script");
+    analytics.textContent = '(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){\n' + '(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n' + 'm=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n' + '})(window,document,\'script\',\'//www.google-analytics.com/analytics.js\',\'ga\');\n' + 'ga(\'create\', \'' + gaTrackingId + '\', \'auto\');\n' + 'ga(\'send\', \'pageview\');\n';
+    head.appendChild(analytics);
   }
 
   function setupBar(err) {
@@ -21,36 +20,37 @@
 
     var isTouchDevice = 'ontouchstart' in document.documentElement;
     var detailsBar = document.querySelector(".details-bar");
-    detailsBar.setAttribute("style", "");
+    
+    if (detailsBar) {
+      detailsBar.setAttribute("style", "");
+      if (isTouchDevice) {
+        detailsBar.classList.add("touch-mode");
+      } else {
+        detailsBar.classList.add("mouse-mode");
+      }
 
-    if(isTouchDevice) {
-      detailsBar.classList.add("touch-mode");
-    }
-    else {
-      detailsBar.classList.add("mouse-mode");
-    }
+      detailsBar.addEventListener("click", function(event) {
+        if (event.target.classList.contains("thimble-button")) {
+          detailsBar.classList.remove("collapsed");
+          event.stopPropagation();
+        }
+      });
 
-    detailsBar.addEventListener("click", function(event) {
-      if (event.target.className == "thimble-button") {
+      detailsBar.addEventListener("click", function(event) {
+        if (event.target.classList.contains("close-details-bar")) {
+          detailsBar.classList.add("collapsed");
+          event.stopPropagation();
+        }
+      });
+
+      document.querySelector(".mouse-mode").addEventListener("mouseenter", function(){
         detailsBar.classList.remove("collapsed");
-        return false;
-      }
-    });
+      });
 
-    detailsBar.addEventListener("click", function(event) {
-      if (event.target.className == "close-details-bar") {
+      document.querySelector(".mouse-mode").addEventListener("mouseleave", function(){
         detailsBar.classList.add("collapsed");
-        return false;
-      }
-    });
-
-    document.querySelector(".mouse-mode").addEventListener("mouseenter", function(){
-      detailsBar.classList.remove("collapsed");
-    });
-
-    document.querySelector(".mouse-mode").addEventListener("mouseleave", function(){
-      detailsBar.classList.add("collapsed");
-    });
+      });
+    }
   }
 
   function injectDetailsBar(metadata, callback) {
@@ -66,13 +66,13 @@
     };
 
     xmlhttp.onreadystatechange = function() {
-      if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+      if (xmlhttp.readyState === XMLHttpRequest.DONE ) {
         var response = xmlhttp.response;
         var textStatus = xmlhttp.statusText;
-        if (xmlhttp.status == 200) {
+        if (xmlhttp.status === 200) {
           document.body.insertBefore(response, document.body.firstChild);
 	   callback(null);
-        } else if (xmlhttp.status == 400) {
+        } else if (xmlhttp.status >= 400) {
 	   callback(textStatus);
         }
       }
@@ -94,48 +94,38 @@
     document.head.appendChild(stylesheet);
   }
 
-  function grep(items, callback) {
-    var filtered = [], len = items.length, i = 0;
-    for (i; i < len; i++) {
-      var item = items[i];
-      var cond = callback(item);
-      if (cond) {
-        filtered.push(item);
-      }
+  var hasDataRemixAttribute = function(element) {
+    var name = element.getAttribute("name");
+    if (!name) {
+      return false;
     }
-    return filtered;
-  }
+    return name.startsWith("data-remix-");
+  };
 
   function getMetadata() {
     var metadata = {};
-    var metaTags = document.getElementsByTagName("meta");
+    var metaElements = document.getElementsByTagName("meta");
+    var remixElements = Array.from(metaElements).filter(hasDataRemixAttribute);
 
-    grep(metaTags, function(metaTag) {
-      return /^data-remix-.+/.test((metaTag).getAttribute("name"));
-    })
-    .forEach(function(metaTag) {
-      var key = metaTag.getAttribute("name").replace(/^data-remix-/, "");
+    remixElements.forEach(function(metaElement) {
+      var key = metaElement.getAttribute("name").replace("data-remix-", "");
 
       // We see if a value for the current attribute already exists.
       // If it does, that means that we already encountered a meta tag
       // with that name which will refer to a more recent value than the
       // current one (we add the most recent values above all other values in
       // the <head> tag's contents).
-      metadata[key] = metadata[key] || metaTag.getAttribute("content");
+      metadata[key] = metadata[key] || metaElement.getAttribute("content");
     });
 
     return metadata;
   }
 
-  function run() {
-    document.addEventListener("DOMContentLoaded", function() {
-      var metadata = getMetadata();
-      injectAnalytics();
-      injectStyleSheets(metadata);
-      injectDetailsBar(metadata, setupBar);
-    });
-  }
-
-  run();  
+  document.addEventListener("DOMContentLoaded", function() {
+    var metadata = getMetadata();
+    injectAnalytics();
+    injectStyleSheets(metadata);
+    injectDetailsBar(metadata, setupBar);
+  });
 
 }(document, document.head));
