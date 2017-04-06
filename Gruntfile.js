a3fbaa0e53134134a80bfcd2aff636ffc1176ab4
mozilla <luke.pacholski@gmail.com>
2017-04-06 10:00:25 -0700

    Gruntfile typo fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,7 +46,7 @@ module.exports = function( grunt ) {
             "PathCache": "editor/scripts/path-cache",
             "constants": "editor/scripts/constants",
             "EventEmitter": "../node_modules/wolfy87-eventemitter/EventEmitter.min",
-            "analytics": "editor/scrips/analytics",
+            "analytics": "editor/scripts/analytics",
             "moment": "../node_modules/moment/min/moment-with-locales.min",
             "gallery": "homepage/scripts/gallery"
           },
