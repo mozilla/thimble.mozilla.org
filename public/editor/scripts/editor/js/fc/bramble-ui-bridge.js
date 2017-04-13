ad0a261aa6cf114d76206a2022b79271770f37c5
Tiago Aguiar <tiagoadjus@hotmail.com>
2017-04-12 23:39:30 -0400

    Implementing mesage propeties to display proper mensage

--- public/editor/scripts/editor/js/fc/bramble-ui-bridge.js
+++ public/editor/scripts/editor/js/fc/bramble-ui-bridge.js
@@ -444,12 +444,13 @@ define(function(require) {
     });
 
     bramble.on("projectDirty", function(){
+      $("#navbar-dirt-file").addClass("hide");
       $("#navbar-save-indicator").removeClass("hide");
-      $("#navbar-save-indicator").text("Saving...");
     });
 
     bramble.on("projectSaved", function(){
       $("#navbar-save-indicator").addClass("hide");
+      $("#navbar-dirt-file").removeClass("hide");
     });
 
     bramble.on("activeEditorChange", function(data) {
