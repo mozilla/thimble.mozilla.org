5d6aeb6818341fb4896fc5f7ff0f536ed8a2429a
Jerry Goguette <jerry_g16@hotmail.com>
2017-04-19 15:35:40 -0400

    Removed Tabs. Using spaces

--- server/lib/snippets.js
+++ server/lib/snippets.js
@@ -13,26 +13,28 @@ class Snippets {
                     name: "Table",
                     title: "Add a Table",
                     id: "snippet-table",
-                    data: '<table> \n' +
-                        '	<tr> \n' +
-                        '		<th></th> \n' +
-                        '		<th></th> \n' +
-                        '	</tr> \n' +
-                        '	<tr> \n' +
-                        '		<td></td> \n' +
-                        '		<td></td> \n' +
-                        '	</tr> \n' +
+                    data: 
+                        '<table> \n' +
+                        '  <tr> \n' +
+                        '    <th></th> \n' +
+                        '    <th></th> \n' +
+                        '  </tr> \n' +
+                        '  <tr> \n' +
+                        '    <td></td> \n' +
+                        '    <td></td> \n' +
+                        '  </tr> \n' +
                         '</table>'
                 },
                 {
                     name: "Definition List",
                     title: "Add a Definition List",
                     id: "snippet-definitionList",
-                    data: '<dl> \n' +
-                        '	<dt></dt> \n' +
-                        '		<dd></dd> \n' +
-                        '	<dt></dt> \n' +
-                        '		<dd></dd> \n' +
+                    data: 
+                        '<dl> \n' +
+                        '  <dt></dt> \n' +
+                        '    <dd></dd> \n' +
+                        '  <dt></dt> \n' +
+                        '    <dd></dd> \n' +
                         '</dl>'
                 }
             ],
@@ -40,16 +42,18 @@ class Snippets {
                     name: "Body Tag",
                     title: "Add a Body Tag",
                     id: "snippet-BodyTag",
-                    data: 'body {\n' +
-                        '     background-color: lightblue;\n' +
+                    data: 
+                        'body {\n' +
+                        '  background-color: lightblue;\n' +
                         '}'
                 },
                 {
                     name: "Paragraph Tag",
                     title: "Add a Paragraph Tag",
                     id: "snippet-ParagraphTag",
-                    data: 'p {\n' +
-                        '     font-size: 20px;\n' +
+                    data: 
+                        'p {\n' +
+                        '  font-size: 20px;\n' +
                         '}'
                 }
             ],
@@ -57,7 +61,8 @@ class Snippets {
                 name: "Basic Function",
                 title: "Add a Basic Function",
                 id: "snippet-BasicFunction",
-                data: 'function functionName(argument) {\n' +
+                data: 
+                    'function functionName(argument) {\n' +
                     '  ....\n' +
                     '}'
             }]
