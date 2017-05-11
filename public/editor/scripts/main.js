7cf77590adc6fd4416f10a45450e0cb14b8b722b
mozilla <luke.pacholski@gmail.com>
2017-05-11 12:33:27 -0700

    Added GA events & longer timeout

--- public/editor/scripts/main.js
+++ public/editor/scripts/main.js
@@ -26,7 +26,7 @@ require.config({
   }
 });
 
-require(["jquery", "bowser"], function($, bowser) {
+require(["jquery", "bowser", "analytics"], function($, bowser, analytics) {
   // Warn users of unsupported browsers that they can try something newer,
   // specifically anything before IE 11 or Safari 8.
   if((bowser.msie && bowser.version < 11) || (bowser.safari && bowser.version < 8)) {
@@ -52,7 +52,7 @@ require(["jquery", "bowser"], function($, bowser) {
 
   Bramble.once("error", onError);
 
-  var errorMessageTimeoutMS = 10000;
+  var errorMessageTimeoutMS = 15000;
 
   setTimeout(function(){
     showLoadingErrorMessage();
@@ -60,10 +60,12 @@ require(["jquery", "bowser"], function($, bowser) {
 
   function showLoadingErrorMessage(){
     $("#spinner-container .taking-too-long").addClass("visible");
+    analytics.event({ category : analytics.eventCategories.TROUBLESHOOTING, action : "Not loading message shown" });
   }
 
   $("button.refresh-browser").on("click",function(){
     window.location.reload(true);
+    analytics.event({ category : analytics.eventCategories.TROUBLESHOOTING, action : "Refresh button clicked" });
   });
 
   Bramble.once("updatesAvailable", function() {
@@ -72,6 +74,7 @@ require(["jquery", "bowser"], function($, bowser) {
 
   function showRefreshAlert(){
     console.log("Thimble has updates - please refresh your browser to get the latest changes.");
+    analytics.event({ category : analytics.eventCategories.TROUBLESHOOTING, action : "Updates available UI shown" });
     $("body").addClass("has-alert-bar");
     $("#serviceworker-warning").removeClass("hide");
   }
