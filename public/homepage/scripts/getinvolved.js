8eb77d586a45ebd79ed68efd3f8b5f9ce5b4694c
mozilla <luke.pacholski@gmail.com>
2017-05-11 22:50:25 -0700

    Addressed a bunch of nits

--- public/homepage/scripts/getinvolved.js
+++ public/homepage/scripts/getinvolved.js
@@ -10,7 +10,7 @@ define(["jquery"], function($) {
 
       var URL = "https://api.github.com/repos/mozilla/thimble.mozilla.org/issues?labels=good%20first%20bug";
 
-      var that =  this;
+      var that = this;
       $.ajax({
         url: URL,
         complete: function(xhr) {
