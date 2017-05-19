405c93beeacc8d2102fb80045be38065ef747905
Rajat Dhyani <contact@rajatdhyani.xyz>
2017-05-19 14:59:10 +0530

    Fix :2084 Added Play Pause feature on Touch Events

--- public/homepage/scripts/features.js
+++ public/homepage/scripts/features.js
@@ -4,6 +4,23 @@ define(["jquery"], function($) {
     init: function(){
       this.feature = $(".feature");
 
+      var touch = "ontouchstart" in document;
+      if (touch){
+          var videoDiv;
+          this.feature.bind('touchstart',function(event){
+             $('video').get(0).pause();
+             $('video', this).get(0).play();
+             var touch = event.touches[0];
+             videoDiv = document.elementFromPoint(touch.pageX,touch.pageY);
+          });
+          this.feature.bind('changedTouches',function(event){
+             var touch = event.touches[0];
+             if (document.elementFromPoint(touch.pageX,touch.pageY) !== videoDiv) {
+                $('video', this).get(0).pause();
+              }
+          });
+       }
+      
       this.feature.mouseenter(function(){
         $('video', this).get(0).play(); 
       });
