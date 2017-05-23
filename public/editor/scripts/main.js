c322bfdf9548d694865ed4a3f2c5f709f13f22ca
mozilla <luke.pacholski@gmail.com>
2017-05-23 13:35:09 -0700

    Capitalization fix

--- public/editor/scripts/main.js
+++ public/editor/scripts/main.js
@@ -47,7 +47,7 @@ require(["jquery", "bowser", "analytics"], function($, bowser, analytics) {
   function onError(err) {
     console.error("[Bramble Error]", err);
     $("#spinner-container").addClass("loading-error");
-    analytics.event({ category : analytics.eventCategories.TROUBLESHOOTING, action : "Editor loading error (black Screen)" });
+    analytics.event({ category : analytics.eventCategories.TROUBLESHOOTING, action : "Editor loading error (Black Screen)" });
   }
 
   // If Bramble fails to load (some browser loading issues cause it to fail),
