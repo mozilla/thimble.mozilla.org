7298b02fa64f4724c8a0061001ff347fc791f6bf
Gideon Thomas <r.gideonthomas@gmail.com>
2017-07-18 15:55:18 -0400

    RE: Add webpack and convert homepage scripts to CommonJS (#2384)
    * Add webpack and convert homepage scripts to CommonJS
    
    * jshint

---
+++ public/lib/keyhandler.js
@@ -0,0 +1,64 @@
+/* globals $: true */
+
+var $ = require("jquery");
+
+// Run the given function `fn` when the key with `keyCode` is pressed down
+function KeyHandler(keyCode, elem, fn) {
+
+  if(typeof keyCode !== "number") {
+    fn = elem;
+    elem = keyCode;
+    keyCode = null;
+  }
+
+  function handler(e) {
+    if(!keyCode) {
+      return fn(e);
+    }
+
+    if(e.which !== keyCode) {
+      return;
+    }
+
+    e.stopPropagation();
+    e.preventDefault();
+    fn(e);
+  }
+
+  if(typeof elem === "function") {
+    fn = elem;
+    elem = document;
+  }
+
+  $(elem).on("keyup", handler);
+
+  this.stop = function() {
+    $(elem).off("keyup", handler);
+  };
+}
+
+// Helper for ESC key handling
+function EscKeyHandler(elem, fn) {
+  KeyHandler.call(this, 27, elem, fn);
+}
+EscKeyHandler.prototype = KeyHandler.prototype;
+EscKeyHandler.prototype.constructor = EscKeyHandler;
+KeyHandler.ESC = EscKeyHandler;
+
+// Helper for Enter key handling
+function EnterKeyHandler(elem, fn) {
+  KeyHandler.call(this, 13, elem, fn);
+}
+EnterKeyHandler.prototype = KeyHandler.prototype;
+EnterKeyHandler.prototype.constructor = EnterKeyHandler;
+KeyHandler.Enter = EnterKeyHandler;
+
+// Helper for any key being pressed to check the title length
+function AnyKeyHandler(elem, fn) {
+  KeyHandler.call(this, elem, fn);
+}
+AnyKeyHandler.prototype = KeyHandler.prototype;
+AnyKeyHandler.prototype.constructor = AnyKeyHandler;
+KeyHandler.Any = AnyKeyHandler;
+
+module.exports = KeyHandler;
--- scripts/start.js
+++ scripts/start.js
@@ -3,7 +3,7 @@
 let runAll = require("npm-run-all");
 let env = require("../server/lib/environment");
 
-let tasks = [ "localize-client" ];
+let tasks = [ "localize-client:webpack" ];
 let options = {
   stdout: process.stdout,
   stderr: process.stderr
