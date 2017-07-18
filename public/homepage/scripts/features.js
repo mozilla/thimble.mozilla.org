e66f8fbd308578bda170287ab5d37eb9b6243920
Gideon Thomas <r.gideonthomas@gmail.com>
2017-07-18 15:55:18 -0400

    RE: Add webpack and convert homepage scripts to CommonJS (#2384)
    * Add webpack and convert homepage scripts to CommonJS
    
    * jshint

--- public/homepage/scripts/features.js
+++ public/homepage/scripts/features.js
@@ -1,24 +1,24 @@
-define(["jquery"], function($) {
-  var features = {
-    init: function(){
-      this.featureEls = $(".video-wrapper");
-      var that = this;
+/* globals $: true */
 
-      if ("ontouchstart" in document){
-        this.featureEls.bind('touchstart',function(event){
-          that.startVideo(event.currentTarget);
-        });
-      }
+var $ = require("jquery");
 
-      this.featureEls.mouseenter(function(){
-        that.startVideo($(this).get(0));
+module.exports = {
+  init: function(){
+    this.featureEls = $(".video-wrapper");
+    var that = this;
+
+    if ("ontouchstart" in document){
+      this.featureEls.bind('touchstart',function(event){
+        that.startVideo(event.currentTarget);
       });
-    },
-    startVideo : function(videoEl){
-      $(".video-wrapper:not(.paused)").addClass("paused").find("video").get(0).pause();
-      $(videoEl).removeClass("paused").find("video").get(0).play();
     }
-  };
 
-  return features;
-});
+    this.featureEls.mouseenter(function(){
+      that.startVideo($(this).get(0));
+    });
+  },
+  startVideo : function(videoEl){
+    $(".video-wrapper:not(.paused)").addClass("paused").find("video").get(0).pause();
+    $(videoEl).removeClass("paused").find("video").get(0).play();
+  }
+};
