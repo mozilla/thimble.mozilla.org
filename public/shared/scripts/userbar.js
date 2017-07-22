9ae16a702f8e8014f6955f34a07cab96d3ccd85d
Gideon Thomas <gideon@mozillafoundation.org>
2017-07-22 16:03:30 -0400

    Webpack the projects-list module

---
+++ public/shared/scripts/userbar.js
@@ -0,0 +1,21 @@
+var PopupMenu = require("./popupmenu");
+
+function createDropdownMenus(exclude) {
+  exclude = exclude || [];
+
+  if(exclude.indexOf("#navbar-logged-in") === -1) {
+    PopupMenu.create("#navbar-logged-in .dropdown-toggle", "#navbar-logged-in .dropdown-content");
+  }
+
+  if(exclude.indexOf("#navbar-locale") === -1) {
+    PopupMenu.create("#navbar-locale .dropdown-toggle", "#navbar-locale .dropdown-content");
+  }
+
+  if(exclude.indexOf("#navbar-help") === -1) {
+    PopupMenu.create("#navbar-help .dropdown-toggle", "#navbar-help .dropdown-content");
+  }
+}
+
+module.exports = {
+  createDropdownMenus: createDropdownMenus
+};
