67ce279a732e3eedc5e3f28c5016e7e704ac70e0
Gideon Thomas <r.gideonthomas@gmail.com>
2017-08-03 18:02:29 -0400

    Webpack the editor module (#2410)

--- public/editor/scripts/editor.js
+++ public/editor/scripts/editor.js
@@ -1,50 +1,58 @@
-define(function(require) {
-  var $ = require("jquery"),
-      UI = require("ui/index"),
-      FileSystemSync = require("filesystem-sync/index"),
-      Project = require("project/index"),
-      BrambleShim = require("bramble-shim"),
-      analytics = require("analytics");
+/* globals $: true */
+var $ = require("jquery");
 
-  var csrfToken = $("meta[name='csrf-token']").attr("content");
+var UI = require("./ui");
+var FileSystemSync = require("./filesystem-sync");
+var Project = require("./project");
+var BrambleShim = require("./bramble-shim");
+var analytics = require("../../shared/scripts/analytics");
 
-  return {
-    csrfToken: csrfToken,
-    create: function(options) {
-      FileSystemSync.init(csrfToken);
+var _csrfToken;
 
-      // Start loading the Bramble editor resources
-      Bramble.load("#webmaker-bramble",{
-        url: options.editorUrl,
-        hideUntilReady: true
-      });
+function create(options) {
+  FileSystemSync.init(_csrfToken);
 
-      // Start loading the project files
-      Project.load(csrfToken, function(err, fileToOpen) {
-        if(err) {
-          console.error("[Thimble Error]", err);
-          $("#spinner-container").addClass("loading-error");
-          analytics.event({ category : analytics.eventCategories.TROUBLESHOOTING, action : "Project loading error (Green screen)" });
-          analytics.exception(err, true);
-          return;
-        }
+  // Start loading the Bramble editor resources
+  Bramble.load("#webmaker-bramble",{
+    url: options.editorUrl,
+    hideUntilReady: true
+  });
 
-        // Now that fs is setup, tell Bramble which root dir to mount
-        // and which file within that root to open on startup.
-        Bramble.mount(Project.getRoot(), fileToOpen);
-      });
-
-      Bramble.once("ready", function(bramble) {
-        analytics.timing({ category: analytics.timingCategories.BRAMBLE, var: "ready Event"});
-
-        // Make sure we don't crash trying to access new APIs not in Bramble's API
-        // before we update the Service Worker cached version we're using.
-        BrambleShim.shimAPI(bramble);
-
-        // For debugging, attach to window.
-        window.bramble = bramble;
-        UI.init(bramble, csrfToken, options.appUrl);
-      });
+  // Start loading the project files
+  Project.load(_csrfToken, function(err, fileToOpen) {
+    if(err) {
+      console.error("[Thimble Error]", err);
+      $("#spinner-container").addClass("loading-error");
+      analytics.event({ category : analytics.eventCategories.TROUBLESHOOTING, action : "Project loading error (Green screen)" });
+      analytics.exception(err, true);
+      return;
     }
-  };
+
+    // Now that fs is setup, tell Bramble which root dir to mount
+    // and which file within that root to open on startup.
+    Bramble.mount(Project.getRoot(), fileToOpen);
+  });
+
+  Bramble.once("ready", function(bramble) {
+    analytics.timing({ category: analytics.timingCategories.BRAMBLE, var: "ready Event"});
+
+    // Make sure we don't crash trying to access new APIs not in Bramble's API
+    // before we update the Service Worker cached version we're using.
+    BrambleShim.shimAPI(bramble);
+
+    // For debugging, attach to window.
+    window.bramble = bramble;
+    UI.init(bramble, _csrfToken, options.appUrl);
+  });
+}
+
+$(function() {
+  _csrfToken = $("meta[name='csrf-token']").attr("content");
 });
+
+module.exports = {
+  get csrfToken() {
+    return _csrfToken;
+  },
+  create: create
+};
--- public/editor/scripts/filesystem-sync/backoff.js
+++ public/editor/scripts/filesystem-sync/backoff.js
@@ -2,26 +2,24 @@
  * Determine a backoff delay (ms) to wait before running the next
  * network operation. Inspired by https://github.com/awslabs/aws-arch-backoff-simulator/blob/master/src/backoff_simulator.py
  */
-define(function(require) {
-  var BACKOFF_BASE_MS = require("constants").BACKOFF_BASE_MS;
-  var BACKOFF_MAX_DELAY_MS = require("constants").BACKOFF_MAX_DELAY_MS;
+var BACKOFF_BASE_MS = require("constants").BACKOFF_BASE_MS;
+var BACKOFF_MAX_DELAY_MS = require("constants").BACKOFF_MAX_DELAY_MS;
 
-  // Returns a random integer between min (included) and max (included)
-  // Using Math.round() will give you a non-uniform distribution!
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-  function _getRandomIntInclusive(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+// Returns a random integer between min (included) and max (included)
+// Using Math.round() will give you a non-uniform distribution!
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function _getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-  function Backoff() {
-    this.iter = 0;
-  }
+function Backoff() {
+  this.iter = 0;
+}
 
-  Backoff.prototype.next = function() {
-    this.iter++;
-    var v = Math.min(BACKOFF_BASE_MS * Math.pow(2, this.iter), BACKOFF_MAX_DELAY_MS);
-    return _getRandomIntInclusive(0, v);
-  };
+Backoff.prototype.next = function() {
+  this.iter++;
+  var v = Math.min(BACKOFF_BASE_MS * Math.pow(2, this.iter), BACKOFF_MAX_DELAY_MS);
+  return _getRandomIntInclusive(0, v);
+};
 
-  return Backoff;
-});
+module.exports = Backoff;
