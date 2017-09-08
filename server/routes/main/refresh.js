2a12b124096b084b5ee7f4f1f69473292ec47621
mozilla <luke.pacholski@gmail.com>
2017-09-08 16:05:27 -0700

    Removed unused template options and updated the twitter account.

--- server/routes/main/refresh.js
+++ server/routes/main/refresh.js
@@ -1,16 +1,5 @@
 var querystring = require("querystring");
 
 module.exports = function(config, req, res) {
-  var qs = querystring.stringify(req.query);
-  if(qs !== "") {
-    qs = "?" + qs;
-  }
-
-  var options = {
-    URL_PATHNAME: "/" + qs,
-    languages: req.app.locals.languages,
-    pageName: "refresh-editor"
-  };
-
-  res.render("refresh.html", options);
+  res.render("refresh.html");
 };
