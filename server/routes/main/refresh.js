9c62a69de29cdf9a93b726cbb21b78f97a06bafe
Gideon Thomas <gideon@mozillafoundation.org>
2017-09-20 16:52:12 -0400

    Set base for refresh and include strings

--- server/routes/main/refresh.js
+++ server/routes/main/refresh.js
@@ -1,4 +1,6 @@
 module.exports = function(config, req, res) {
   res.removeHeader("X-Frame-Options");
-  res.render("refresh.html");
+  res.render("refresh.html", {
+    appURL: config.appURL
+  });
 };
