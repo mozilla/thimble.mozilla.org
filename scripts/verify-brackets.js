d88b7eee7f47ee5f9d0b51cc543857154db3fc66
Michael <m.pierre@live.ca>
2017-10-03 15:03:29 -0400

    Ran npm run  test:lint:fix

--- scripts/verify-brackets.js
+++ scripts/verify-brackets.js
@@ -2,10 +2,10 @@ const config = require("../server/routes/config");
 const isReachable = require("is-reachable");
 var brambleServer = config.editorHOST;
 
-
 isReachable(brambleServer).then(reachable => {
-  
-  if(!reachable) {
-    console.warn("Error: Bramble server is not running. Please end this process and run the Brackets server with: npm start");
+  if (!reachable) {
+    console.warn(
+      "Error: Bramble server is not running. Please end this process and run the Brackets server with: npm start"
+    );
   }
 });
