80da4e1c6628091f55a7f1e4343dad3203180f61
Pomax <pomax@nihongoresources.com>
2018-09-04 12:02:21 -0700

    linting

--- server/routes/main/homepage.js
+++ server/routes/main/homepage.js
@@ -1,4 +1,4 @@
-var moment = require('moment');
+var moment = require("moment");
 var querystring = require("querystring");
 
 module.exports = function(config, req, res) {
@@ -10,8 +10,8 @@ module.exports = function(config, req, res) {
   }
 
   // make sure the glitch move date is localized:
-  moment.locale('en');
-  var migrationDate =  moment(config.glitch.migrationDate);
+  moment.locale("en");
+  var migrationDate = moment(config.glitch.migrationDate);
   migrationDate = migrationDate.locale(locale).format("LL");
 
   var options = {
