4473c3ae137511271239eeda9d48a016c8e8dc34
tem44 <35421983+tem44@users.noreply.github.com>
2018-09-22 13:30:16 +0100

    Update invalidate.js

--- scripts/invalidate.js
+++ scripts/invalidate.js
@@ -1,33 +1,33 @@
 /**
- * This file is a short script that invalidates
- * the cloudfront edge caches for Thimble whenever
- * we deploy a new version.
- */
+* This file is a short script that invalidates
+* the cloudfront edge caches for Thimble whenever
+* we deploy a new version.
+*/
 var AWS = require("aws-sdk");
 AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
 var cloudfront = new AWS.CloudFront();
 var params = {
-  DistributionId: process.env.CLOUDFRONT_DISTRIBUTION_ID,
-  InvalidationBatch: {
-    CallerReference: Date.now().toString(),
-    Paths: {
-      Quantity: 1,
-      // We invalidate everything
-      Items: ["/*"]
+    DistributionId: process.env.CLOUDFRONT_DISTRIBUTION_ID,
+    InvalidationBatch: {
+        CallerReference: Date.now().toString(),
+        Paths: {
+            Quantity: 1,
+            // We invalidate everything
+            Items: ["/*"]
+        }
     }
-  }
 };
 
-cloudfront.createInvalidation(params, function(err, data) {
-  if (err) {
-    console.log(err, err.stack);
-    return process.exit(1);
-  }
+cloudfront.createInvalidation(params, function (err, data) {
+    if (err) {
+        console.log(err, err.stack);
+        return process.exit(1);
+    }
 
-  console.log("Successfully invalidated CloudFront!\n", data);
-  process.exit(0);
+    console.log("Successfully invalidated CloudFront!\n", data);
+    process.exit(0);
 });
